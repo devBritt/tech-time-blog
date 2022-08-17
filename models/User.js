9b093bb154639c1616dba04f6f32285dd688acfc
Britt Carlton <brittanyrc95@gmail.com>
2022-08-17 15:07:46 -0400

    Create User model

---
+++ models/User.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+// create User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+// define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {
+            // set up beforeCreate lifecycle hook
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.checkPassword, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle hook
+            async beforeUpdate(updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
+        // pass in sequelize connection
+        sequelize,
+        // don't automatically create timestamps
+        timestamps: false,
+        // don't pluralize db name
+        freezeTableName: true,
+        // use underscores
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
