7eb8bcb104fe1539c143d23e08083b8f13257477
Britt Carlton <brittanyrc95@gmail.com>
2022-08-18 13:50:44 -0400

    Create update user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -81,7 +81,25 @@ router.post('/', (req, res) => {
 
 
 // PUT /api/users/:id
-
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // DELETE /api/users/:id
 
