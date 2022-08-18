9cd247396e016a5b59e4d91f6d599936f8b39f70
Britt Carlton <brittanyrc95@gmail.com>
2022-08-18 13:54:11 -0400

    Create delete user by id route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,6 +102,22 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/:id
-
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
