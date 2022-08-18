34c62b9db0313031eb7949b0bc599b532db47f4d
Britt Carlton <brittanyrc95@gmail.com>
2022-08-18 15:22:30 -0400

    Create update and delete post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -34,7 +34,39 @@ router.get('/', (req, res) => {
 });
 
 // GET /api/posts/:id
-
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // POST /api/posts
 router.post('/', (req, res) => {
@@ -52,8 +84,46 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/posts/:id
-
+router.put('/:id', (req, res) => {
+    Post.update({
+        title: req.body.title
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // DELETE /api/posts/:id
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
