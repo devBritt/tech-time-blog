de455f23e3431f5578d309b64df2881e35ba6f64
Britt Carlton <brittanyrc95@gmail.com>
2022-08-18 13:45:59 -0400

    Create get user by id route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,7 +14,39 @@ router.get('/', (req, res) => {
 });
 
 // GET /api/users/:id
-
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // POST /api/users
 router.post('/', (req, res) => {
