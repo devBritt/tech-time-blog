0a05bdf2d4e9f3e9984651d84001338630a954bb
Britt Carlton <brittanyrc95@gmail.com>
2022-08-23 11:39:43 -0400

    Create dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,4 +1,51 @@
+const { Post, Comment, User } = require('../models');
 const router = require('express').Router();
+
+// view dashboard
+router.get('/', (req, res) => {
+    // verify user is logged in
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+        return;
+    }
+    
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at', 'post_id', 'user_id'],
+                include: [{
+                    model: User,
+                    attributes: ['username']
+                }]
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// view new/edit post page
 
 
 module.exports = router;
