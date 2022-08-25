4f7fc4b480c76a474dc1175f86c402ef10425aca
Britt Carlton <brittanyrc95@gmail.com>
2022-08-25 10:35:30 -0400

    Fix sequelize onDelete for model associations

--- models/index.js
+++ models/index.js
@@ -5,30 +5,30 @@ const Comment = require('./Comment');
 // create model associations
 // associate post with user via user_id
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
 });
 // associate user with post via user_id
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'cascade'
+    foreignKey: 'user_id'
 });
 // associate user with comment via user_id
 Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+// associate post with comment via post_id
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+// associate comment with user via user_id
+User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'cascade'
 });
 // associate post with comment via post_id
-Comment.belongsTo(Post, {
+Post.hasMany(Comment, {
     foreignKey: 'post_id',
     onDelete: 'cascade'
-});
-// associate comment with user via user_id
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
-// associate post with comment via post_id
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
